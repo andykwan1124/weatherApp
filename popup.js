3ff2f8f668483b628a23e1bb0e0673b74e2e82c2
andykwan1124 <woody.andygrant@gmail.com>
2019-10-13 20:54:11 -0700

    Hover feature and data of the coming 3 hours

--- popup.js
+++ popup.js
@@ -1,32 +1,14 @@
-// function check() {
-    
-// }
-
-// function main() {
-//     check();
-// }
-
-// if( document.readyState !== 'loading' ) {
-//     console.log( 'document is already ready, just execute code here' );
-//     myInitCode();
-// } else {
-//     document.addEventListener('DOMContentLoaded', function () {
-//         console.log( 'document was not ready, place code here' );
-//         myInitCode();
-//     });
-// }
-
-
 const myInitCode = () => {
     if ("geolocation" in navigator) {
         console.log('API available');
         navigator.geolocation.getCurrentPosition(position => {
         console.log(position.coords.latitude);
-        var lat = position.coords.latitude;
         console.log(position.coords.longitude);
-        var long = position.coords.longitude;
-        onClick(lat,long);
+        findTime();
+        onClick(position.coords.latitude,position.coords.longitude);
         })
+        
+        
     } else {
         console.log('API not available');
     }
@@ -35,18 +17,11 @@ const myInitCode = () => {
 }
 
 const onClick = (lat, long) => { 
-    // var myLat = document.createElement("div");
-    // myLat.textContent = lat;
-    // document.body.appendChild(myLat);
-    // var myLong = document.createElement("div");
-    // myLong.textContent = long;
-    // document.body.appendChild(myLong);
     $.getJSON(
         `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c486de8f81ddf8d7a67e10d28ec20a9f/${lat},${long}?units=si`,
         function(data) {
-            var currentTemp = document.createElement("div");
+            var currentTemp = document.querySelector('[currentTemp]');
             currentTemp.textContent = data.currently.apparentTemperature + `°C`;
-            document.getElementById("tempData").appendChild(currentTemp);
             console.log(data);
         
             var currentIcon = data.currently.icon.replace(/-/g, "_").toUpperCase();
@@ -55,7 +30,54 @@ const onClick = (lat, long) => {
                                         {"resizeClear": true})
             skycons.add(document.getElementById("icon1"), Skycons[currentIcon]);
             skycons.play();
+
+            var firstData = document.querySelector('[firstData]');
+            firstData.textContent = "LOL";
+
+            var secondData = document.querySelector('[secondData]');
+            secondData.textContent = "smh";
+
+            for (var i = 1; i <= 3; i++) {
+                var currentIcon = data.hourly.data[i].icon.replace(/-/g, "_").toUpperCase();
+                console.log(currentIcon);
+                var skycons = new Skycons({color: "black"},
+                                            {"resizeClear": true})
+                skycons.add(document.getElementById(`${i}icon`), Skycons[currentIcon]);
+                skycons.play();
+            }
+
+            for (var i = 1; i <= 3; i++) {
+                var futureTemp = document.getElementById(`${i}Data`);
+                futureTemp.textContent = data.hourly.data[i].temperature + `°C`;
+            }
         })
+}
+
+const findTime = () => {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var suffix = "AM";
+
+    if (hours >= 12) {
+        suffix = "PM";
+        hours = hours - 12;
+    }
+
+    if (hours == 0) {
+        hours = 12;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    var currTime = document.querySelector('[currTime]');
+    currTime.textContent = `${hours}:${minutes} ${suffix}`;
+
+    for (var i = 1; i <= 3; i++) {
+        var futureTime = document.getElementById(`${i}Time`);
+        futureTime.textContent = `${hours+i}:00 ${suffix}`
+    }
 }
 
 myInitCode();
